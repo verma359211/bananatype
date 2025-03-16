ddefe161bf20bb68dc48386f9ca7d8177fdde401
verma359211 <vermachandan2912003@gmail.com>
2025-03-16 23:39:15 +0530

    API update

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -9,7 +9,12 @@ export async function GET() {
     await connectDb();
     try{
         const users = await User.find().sort({topSpeed:-1});
-        return NextResponse.json({message:"leaderboard found",users},{status:200})
+        return NextResponse.json({ message: "leaderboard found", users }, {
+            status: 200, headers: {
+                "Cache-Control": "no-store", // Prevents Next.js from caching
+                "Content-Type": "application/json"
+            },
+        })
     }catch(error : unknown){
         return NextResponse.json({ message: "failed to load leaderboard \n\n"+error }, {status:500})
     }
