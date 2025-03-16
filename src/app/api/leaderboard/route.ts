b27f9e1742c6189e08b9bec5e21eb87dd6eb6369
verma359211 <vermachandan2912003@gmail.com>
2025-03-17 01:31:56 +0530

    API

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -3,6 +3,7 @@ import connectDb from "@/dbconfig/dbconfig";
 import User from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 
+export const fetchCache = "force-no-store";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: NextRequest) {
@@ -12,7 +13,17 @@ export async function GET(request: NextRequest) {
         const users = await User.find({});
         // console.log("Fetched Users:", users);
         return NextResponse.json(
-					{ message: "leaderboard found", users },{status: 200,});
+					{ message: "leaderboard found", users },
+					{
+						status: 200,
+						headers: {
+							"Cache-Control":
+								"no-store, no-cache, must-revalidate, proxy-revalidate", // No cache
+							Pragma: "no-cache",
+							Expires: "0",
+						},
+					}
+				);
     }catch(error : unknown){
         return NextResponse.json({ message: "failed to load leaderboard \n\n"+error }, {status:500})
     }
