f3618a792f78f5b9b00957b6650afc6e991f27db
verma359211 <vermachandan2912003@gmail.com>
2025-03-16 22:37:12 +0530

    leaderboard API update01

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,19 +1,17 @@
 import { connectDb } from "@/dbconfig/dbconfig";
 // import { getDataFromToken } from "@/lib/getDataFromToken";
 import User from "@/models/user.model";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 connectDb();
 
 //leaderboard api send the top 10 users with thier username , topSpeed and avgSpeed and testAttempted 
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(request:NextRequest){
+export async function GET(){
     try{
         const users = await User.find().sort({topSpeed:-1});
         return NextResponse.json({message:"leaderboard found",users},{status:200})
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
-    }catch(error:any){
-        return NextResponse.json({message:"failed to load leaderboard"},{status:500})
+    }catch(error : unknown){
+        return NextResponse.json({ message: "failed to load leaderboard \n\n"+error }, {status:500})
     }
 }
--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -1,30 +1,56 @@
+// import mongoose from "mongoose";
+
+// export async function connectDb(): Promise<void> {
+// 	try {
+// 		if (!process.env.MONGODB_URL) {
+// 			throw new Error("MONGO_URL is not defined in the environment variables");
+// 		}
+
+// 		await mongoose.connect(process.env.MONGODB_URL);
+
+// 		const connection = mongoose.connection;
+
+// 		connection.on("connected", () => {
+// 			console.log("Connected to MongoDB");
+// 		});
+
+// 		connection.on("error", (error: Error) => {
+// 			console.log("Error connecting to MongoDB");
+// 			console.error(error);
+// 			process.exit(1);
+// 		});
+// 	} catch (error: unknown) {
+// 		console.log("Something went wrong while connecting to DB");
+// 		console.error(error);
+// 	}
+// }
+
 import mongoose from "mongoose";
 
-export async function connectDb():Promise<void>{
-    try{
-        if (!process.env.MONGODB_URL) {
-            throw new Error("MONGO_URL is not defined in the environment variables");
-          }
+type ConnectionObject = {
+	isConnected?: number;
+};
 
-          await mongoose.connect(process.env.MONGODB_URL);
+const connection: ConnectionObject = {};
 
+export async function connectDb(): Promise<void> {
+  console.log("Going for check");
+	// Check if we have a connection to the database or if it's currently connecting
+	if (connection.isConnected) {
+		console.log("Already connected to the database");
+		return;
+	}
+  console.log("Going for database connection");
+	try {
+		// Attempt to connect to the database
+		const db = await mongoose.connect(process.env.MONGODB_URL || "", {});
+		connection.isConnected = db.connections[0].readyState;
 
-          const connection = mongoose.connection;
+		console.log("Database connected successfully");
+	} catch (error) {
+		console.error("Database connection failed:", error);
 
-          connection.on("connected", () => {
-            console.log("Connected to MongoDB");
-          });
-
-          connection.on("error",(error:Error) => {
-            console.log("Error connecting to MongoDB");
-            console.error(error);
-            process.exit(1);
-            
-          })
-
-    }catch(error:unknown)
-    {
-        console.log('Something went wrong while connecting to DB');
-        console.error(error);
-    }
+		// Graceful exit in case of a connection error
+		process.exit(1);
+	}
 }
