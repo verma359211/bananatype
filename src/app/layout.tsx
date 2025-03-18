47844fc1c5c52a190aed2b52b1d41894360779a3
verma359211 <vermachandan2912003@gmail.com>
2025-03-18 06:37:10 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,11 @@ import "./globals.css";
 export const fetchCache = "force-no-store";
 
 export const metadata: Metadata = {
-  title: "bananaType",
-  description: "A typing test game",
+	title: "bananaType",
+	description: "A typing test game",
+	icons: {
+		icon: "/favicon.ico", // Path to my favicon file
+	},
 };
 
 export default function RootLayout({
@@ -18,20 +21,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body> 
-      {/* <ThemeProvider
+		<html lang="en">
+			<head>
+				<meta name="description" content="A typing test game" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				{/* Explicit favicon link */}
+				<link rel="icon" href="/favicon.ico" />
+			</head>
+			<body>
+				{/* <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           > */}
-        {/* <Navbar /> */}
-        {children}
-        {/* <Footer /> */}
-        <Analytics />
-        {/* </ThemeProvider> */}
-      </body>
-    </html>
-  );
+				{/* <Navbar /> */}
+				{children}
+				{/* <Footer /> */}
+				<Analytics />
+				{/* </ThemeProvider> */}
+			</body>
+		</html>
+	);
 }
