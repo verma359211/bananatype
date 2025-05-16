3de9277633efed4251e2d25886eb4c303117bb2b
verma359211 <vermachandan2912003@gmail.com>
2025-05-17 01:31:16 +0530

    commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,18 +4,28 @@ import { MessageSquare, Github, Twitter } from "lucide-react";
 export default function Footer() {
 	return (
 		<footer className="p-4 flex flex-wrap justify-center gap-3 sm:gap-4 text-xs sm:text-sm text-zinc-500">
-			<Link href="#" className="hover:text-zinc-300 flex items-center gap-1">
+			<Link
+				href="https://www.chandanverma.tech/"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="hover:text-zinc-300 flex items-center gap-1"
+			>
 				<MessageSquare className="h-3 w-3 sm:h-4 sm:w-4" />
 				<span>contact</span>
 			</Link>
-			<Link href="#" className="hover:text-zinc-300 flex items-center gap-1">
+			{/* <Link href="#" className="hover:text-zinc-300 flex items-center gap-1">
 				<span>support</span>
-			</Link>
-			<Link href="#" className="hover:text-zinc-300 flex items-center gap-1">
+			</Link> */}
+			<a
+				href="https://github.com/verma359211/bananatype"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="hover:text-zinc-300 flex items-center gap-1"
+			>
 				<Github className="h-3 w-3 sm:h-4 sm:w-4" />
 				<span>github</span>
-			</Link>
-			<Link href="#" className="hover:text-zinc-300 flex items-center gap-1">
+			</a>
+			{/* <Link href="#" className="hover:text-zinc-300 flex items-center gap-1">
 				<MessageSquare className="h-3 w-3 sm:h-4 sm:w-4" />
 				<span>discord</span>
 			</Link>
@@ -31,7 +41,7 @@ export default function Footer() {
 			</Link>
 			<Link href="#" className="hover:text-zinc-300">
 				privacy
-			</Link>
+			</Link> */}
 		</footer>
 	);
 }
